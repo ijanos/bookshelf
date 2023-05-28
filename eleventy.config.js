9f189d294a47e815ceba451e51ef6b6e53b10959
János Illés <ijanos@gmail.com>
2023-05-28 12:36:51 +0200

    build iso date out of parts instead of slicing full date time strings

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,25 @@
 const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 
+function isoDate() {
+  function join(t, a, s) {
+    function format(m) {
+       const f = new Intl.DateTimeFormat('en', m);
+       return f.format(t);
+    }
+    return a.map(format).join(s);
+ }
+
+ const a = [{year: 'numeric'}, {month: '2-digit'}, {day: '2-digit'}];
+ return join(new Date, a, '-');
+}
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
     watch: ['_site/**/*.css',],
   });
 
-  eleventyConfig.addGlobalData("buildDate", () => new Date());
+  eleventyConfig.addGlobalData("buildDate", isoDate);
 
   eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
 
